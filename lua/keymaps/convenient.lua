dd28b5fbf44a33821f60bf819d31e031a7a6c8ad
STG <stg737467@gmail.com>
2025-12-10 13:15:41 +0900

    fix(keymaps): vim.ui.openが上手く動かない問題を回避
    問題: vim.ui.open でURLが開けない
    原因: vim.system({"cmd","/c",コマンド}) をするとcmdが
          code: 1,stderr: "The syntax of the command is incorrect.\r\n"
          で終了する
    解決策: とりあえずcmdの代わりにnuを使う

--- lua/keymaps/convenient.lua
+++ lua/keymaps/convenient.lua
@@ -7,7 +7,7 @@ k("n","<c-w>N","<cmd>botright split | enew | set bufhidden=delete<cr>")
 
 -- 検索系
 local search = function(str)
-    return vim.ui.open("https://google.com/search?q=" .. str)
+    return vim.system({"nu","--commands","start https://google.com/search?q=" .. str})
 end
 k("v","gs",function()
     local v_range = table.concat(vim.fn.getregion(vim.fn.getpos("."), vim.fn.getpos("v"), { type = vim.fn.mode() }),"")

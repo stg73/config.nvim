1734dc43a7dd000a39f699510e7517b547b3cd82
STG <stg737467@gmail.com>
2025-12-10 10:45:18 +0900

    refactor(highlights): 丸括弧の省略のため関数をカリー化

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,18 +1,18 @@
-local h = vim.api.nvim_set_hl
+local h = require("tbl").curry3(vim.api.nvim_set_hl)(0)
 
-h(0,'normalnc',{bg = 'none'})
-h(0,'@keyword',{fg = '#9d7cd8'})
-h(0,'keyword',{fg = '#ae8de9'})
-h(0,'constant',{fg = '#dd8d53'})
-h(0,'comment',{fg = '#676f9a'})
-h(0,'string',{fg = '#8dbd59'})
-h(0,'@punctuation.bracket',{fg = '#cc6666'})
+h 'normalnc' { bg = 'none' }
+h '@keyword' { fg = '#9d7cd8' }
+h 'keyword' { fg = '#ae8de9' }
+h 'constant' { fg = '#dd8d53' }
+h 'comment' { fg = '#676f9a' }
+h 'string' { fg = '#8dbd59' }
+h '@punctuation.bracket' { fg = '#cc6666' }
 
 -- neovim側の色
-h(0,'Question',{fg = '#5880d5'}) -- できるだけ目立たないように
-h(0,'visual',{bg = '#4a5679'})
-h(0,'linenr',{fg = '#565f89'})
-h(0,'linenc',{link = 'linenr'})
-h(0,'statuslinenc',{link = 'linenr'})
-h(0,'winseparator',{link = 'linenr'})
-h(0,'statusline',{link = 'linenr'})
+h 'Question' { fg = '#5880d5' } -- できるだけ目立たないように
+h 'visual' { bg = '#4a5679' }
+h 'linenr' { fg = '#565f89' }
+h 'linenc' { link = 'linenr' }
+h 'statuslinenc' { link = 'linenr' }
+h 'winseparator' { link = 'linenr' }
+h 'statusline' { link = 'linenr' }

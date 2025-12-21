ff548300abd5f5e75e67cca831ef44d1649f9110
STG <stg737467@gmail.com>
2025-12-21 18:52:59 +0900

    improve(syntax): nu のハイライトグループを追加

--- syntax/nu.lua
+++ syntax/nu.lua
@@ -1,12 +1,19 @@
 vim.cmd([[
     syntax match nuVarMember '\v(\$\S+\.)@<=\S+' contained containedin=nuVar
+    syntax match nuPipe '\v\|'
+    syntax match nuBracket '\v[}{]'
+    syntax match nuProperty '\v\w+:@='
 ]])
 
 local l = require("highlights").link
 
 l "nuVarMember" "@variable.member"
+l "nuPipe" "@punctuation.delimiter"
+l "nuBracket" "@punctuation.bracket"
+l "nuProperty" "@property"
 
 l "nuVar" "@variable"
 l "nuString" "@string"
 l "nuFlag" "@variable.member"
 l "nuCmd" "@function"
+l "nuCondi" "@keyword.conditional"

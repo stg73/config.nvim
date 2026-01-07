c5e68f772dba080036934a2d585e127898f69619
STG <stg737467@gmail.com>
2026-01-07 12:40:27 +0900

    refactor(keymaps): 冗長な記述を修正

--- lua/keymaps/char.lua
+++ lua/keymaps/char.lua
@@ -1,35 +1,44 @@
-local k = vim.keymap.set
+local set = vim.keymap.set
 
--- 文字を楽に入力 キーボードでやるべきことなので 40%キーボードにしたらなくすかもしれない
-k({"n","i","v","c","t","o"},"<leader>",'') -- 以下を不自由なく使うために必要
-k({"n","i","v","c","t","o"},"<leader><esc>",'') -- 以下を不自由なく使うために必要
-k({"n","i","v","c","t","o"},"<leader>i","#") -- Igeta 井桁
-k({"n","i","v","c","t","o"},"<leader>p","%") -- Percent sign
-k({"n","i","v","c","t","o"},"<leader>k","*",{remap = true}) -- asterisK
-k({"n","i","v","c","t","o"},"<leader>c","^") -- Circumflex
-k({"n","i","v","c","t","o"},"<leader>t","~") -- Tilde
-k({"n","i","v","c","t","o"},"<leader>a","&") -- Ampersand
-k({"n","i","v","c","t","o"},"<leader>d","$") -- Dollar mark
-k({"n","i","v","c","t","o"},"<leader>v","|") -- Vertical bar
-k({"n","i","v","c","t","o"},"<leader>x","!") -- EXclamation mark
-k({"n","i","v","c","t","o"},"<leader>q","?") -- Question mark
-k({"n","i","v","c","t","o"},"<leader>-","-") -- -
-k({"n","i","v","c","t","o"},"<leader>h","/") -- slasH
-k({"n","i","v","c","t","o"},"<leader>b","\\") -- Back slash
-k({"n","i","v","c","t","o"},"<leader>u","_") -- Under bar
-k({"n","i","v","c","t","o"},"<leader>;",":") -- 形が:に似ている
-k({"i","v","t","o"},"\t","<cmd>set noexpandtab<cr><cmd>set shiftwidth=8<cr>\t<cmd>set expandtab<cr><cmd>set shiftwidth=4<cr>")
-k("i","<leader>r","<cr>") -- Return
-k({"i","t","c"},"<c-q>","'") -- Quote scheme用
+set({"i","v","t","o"},"\t","<cmd>set noexpandtab<cr><cmd>set shiftwidth=8<cr>\t<cmd>set expandtab<cr><cmd>set shiftwidth=4<cr>")
+set("i","<leader>r","<cr>") -- Return
+set({"i","t","c"},"<c-q>","'") -- Quote scheme用
 
--- nuMber 大西配列のホーム行の順番
-k({"n","i","v","c","t","o"},"<leader>me","0")
-k({"n","i","v","c","t","o"},"<leader>mi","1")
-k({"n","i","v","c","t","o"},"<leader>ma","2")
-k({"n","i","v","c","t","o"},"<leader>mo","3")
-k({"n","i","v","c","t","o"},"<leader>m-","4")
-k({"n","i","v","c","t","o"},"<leader>mk","5")
-k({"n","i","v","c","t","o"},"<leader>mt","6")
-k({"n","i","v","c","t","o"},"<leader>mn","7")
-k({"n","i","v","c","t","o"},"<leader>ms","8")
-k({"n","i","v","c","t","o"},"<leader>mh","9")
+local keys = {
+    ["<esc>"] = '', -- 以下を不自由なく使うために必要
+    i = "#", -- Igeta 井桁
+    p = "%", -- Percent sign
+    k = "*", -- asterisK
+    c = "^", -- Circumflex
+    t = "~", -- Tilde
+    a = "&", -- Ampersand
+    d = "$", -- Dollar mark
+    v = "|", -- Vertical bar
+    x = "!", -- EXclamation mark
+    q = "?", -- Question mark
+    ["-"] = "-", -- -
+    h = "/", -- slasH
+    b = "\\", -- Back slash
+    u = "_", -- Under bar
+    [";"] = ":", -- 形が:に似ている
+
+    -- nuMber 大西配列のホーム行の順番
+    me = "0",
+    mi = "1",
+    ma = "2",
+    mo = "3",
+    ["m-"] = "4",
+    mk = "5",
+    mt = "6",
+    mn = "7",
+    ms = "8",
+    mh = "9",
+}
+
+local function keybord(x,y)
+    set({"n","i","v","c","t","o"},"<leader>" .. x,y,{ remap = true })
+end
+
+vim.tbl_map(function(key)
+    keybord(key,keys[key])
+end,vim.tbl_keys(keys))

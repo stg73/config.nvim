fcfff75b2cabfff1695da4197a8a9331cc5dd9bc
STG <stg737467@gmail.com>
2026-01-07 11:41:12 +0900

    refactor(keymaps)!: bracket を移動

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,5 +1,34 @@
+local M = {}
+
+local set = vim.keymap.set
+
+function M.bracket(b_start,b_end)
+    set("i",b_start,b_start .. b_end .. "<c-g>U<left>")
+    set({"c","t"},b_start,b_start .. b_end .. "<left>")
+    set({"i","c","t"},"<leader>" .. b_start,b_start)
+end
+
 require("keymaps.char")
-require("keymaps.bracket")
 require("keymaps.keyboard")
 require("keymaps.convenient")
 require("keymaps.hoge")
+
+-- 閉括弧の自動入力
+local brackets = {
+    {"{","}"},
+    {"[","]"},
+    {"<",">"},
+    -- 日本語
+    {"「","」"},
+    {"『","』"},
+    {"【","】"},
+}
+
+vim.tbl_map(function(t)
+    M.bracket(t[1],t[2])
+end,brackets)
+
+set('i','<c-b>','()<c-g>U<left>') set({'c','t'},'<c-b>','()<left>') -- Bracket
+set('i','<c-d>','""<c-g>U<left>') set({'c','t'},'<c-d>','"<left>"') -- Double quote
+
+return M

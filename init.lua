cd77ab89e33584ed1e680a85c3be2216cf5313b8
STG <stg737467@gmail.com>
2026-03-26 15:15:27 +0900

    feat: "nvim.undotree"を使う

--- init.lua
+++ init.lua
@@ -32,11 +32,12 @@ require("options").setup()
 require("env").setup()
 require("ui2").setup()
 
-require("ghosttext").start()
-
 -- プラグイン
 vim.keymap.set({"o","n","v"},"<leader>j",require("select_position").opt().set_cursor)
 vim.keymap.set({"o","n","v"},"<leader><leader>",require("select_position").opt({ character = "\\s" }).set_cursor)
+
+require("ghosttext").start()
+vim.cmd.packadd("nvim.undotree")
 
 -- デフォルトプラグインを無効化
 tbl.map(function(plugin) vim.g["loaded_" .. plugin] = true end)({

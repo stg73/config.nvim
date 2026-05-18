a2b38eecc4feed1f0a3895e216aaa101304786f5
STG <stg737467@gmail.com>
2026-05-18 11:47:46 +0900

    refactor: nvimモジュールをシンプルに
    問題: require("nvim") に時間がかかる
    解決策:
      原因となっていた複雑なパース処理を単純化
      変更点の例: `nvim.get.proc` -> `nvim.get_proc`, `nvim.buf.get.name` -> `nvim.buf.get_name`

--- lua/nvim.lua
+++ lua/nvim.lua
@@ -1,65 +1,21 @@
 local nvim = {}
 
-local api_words = {
+local topics = {
     "win",
     "buf",
     "tabpage",
-    "current",
-    "create",
-    "del",
-    "get",
-    "set",
-    "list",
-    "call",
-    "parse",
-    "open",
-    "is",
 }
 
-local function parse_name(pattern) return function(name)
-    local t = {}
-    local function loop(str)
-        local s,e = regex.find("^(" .. pattern ..  ")_")(str)
-        if s then
-            local x,y = string.sub(str,s,e - 1),string.sub(str,e + 1)
-            table.insert(t,x)
-            if y ~= "" then
-                return loop(y)
-            end
-        else
-            table.insert(t,str)
-        end
+for _name,fn in pairs(vim.api) do
+    local name = regex.remove("^nvim_")(_name)
+    local s,e = regex.find("^(" .. table.concat(topics,"|") .. ")")(name)
+    if s and e then
+        local topic,rest = string.sub(name,s,e),string.sub(name,e + 2)
+        nvim[topic] = nvim[topic] or {}
+        nvim[topic][rest] = fn
+    else
+        nvim[name] = fn
     end
-    loop(name)
-    return t
-end end
-local parse_api_name = parse_name(table.concat(api_words,"|"))
-
-local function set_table(tbl,list)
-    local function loop(i,t)
-        local is_key = list[i + 1] ~= nil
-        if is_key then
-            local is_table = list[i + 2] ~= nil
-            if is_table then
-                if not t[list[i]] then
-                    t[list[i]] = {}
-                end
-                return loop(i + 1,t[list[i]])
-            else
-                return loop(i + 1,t)
-            end
-        else
-            t[list[i - 1]] = list[i]
-        end
-    end
-
-    loop(1,tbl)
-end
-
-for name,fn in pairs(vim.api) do
-    local parsed = parse_api_name(regex.remove("nvim_")(name))
-    table.insert(parsed,fn)
-    set_table(nvim,parsed)
 end
 
 return nvim
